4b38e78a4ab792e7a1059d18c5d3b76520e06916
Michał Dębek <michal.debek@silksh.pl>
2020-04-19 20:41:59 +0200

    communication between parent and children components

--- main.js
+++ main.js
@@ -32,10 +32,6 @@ Vue.component('product', {
             <button v-on:click="addToCart"
                     :disabled="!inStock"
                     :class="{ disabledButton: !inStock }">Add to Cart</button>
-
-            <div class="cart">
-                <p> Cart {{ cart }}</p>
-            </div>
         </div>
     </div>
     `,
@@ -60,13 +56,12 @@ Vue.component('product', {
                         variantQuantity: 0,
                     }
                 ],
-                cart: 0,
             }
         )
     },
     methods: {
         addToCart() {
-            this.cart += 1;
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
         updateProduct(index) {
             this.selectedVariant = index;
@@ -94,6 +89,12 @@ Vue.component('product', {
 const app = new Vue({
     el: '#app',
     data: {
-        premium: true
+        premium: true,
+        cart: [],
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        }
     }
 });
